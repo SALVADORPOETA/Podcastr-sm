2cc1d7b3e5a14575dbadd47f89da07c9d42a2199
Salvador Poeta <dsmlhidroponia@gmail.com>
2025-07-11 02:24:11 -0600

    separate components client and server

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,56 +1,17 @@
-'use client'
-
-import EmptyState from '@/components/EmptyState'
+import React, { Suspense } from 'react'
+import DiscoverClient from './DiscoverClient'
 import LoaderSpinner from '@/components/LoaderSpinner'
-import PodcastCard from '@/components/PodcastCard'
-import Searchbar from '@/components/Searchbar'
-import { api } from '@/convex/_generated/api'
-import { useQuery } from 'convex/react'
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
 
-const Discover = () => {
-  const searchParams = useSearchParams()
-  const search = searchParams.get('search') || ''
-
-  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
-    search,
-  })
-
+export default function DiscoverPage() {
   return (
-    <div className="flex flex-col gap-9">
-      <Searchbar />
-      <div className="flex flex-col gap-9">
-        <h1 className="text-20 font-bold text-white-1">
-          {!search ? 'Discover Trending Podcasts' : 'Search results for: '}{' '}
-          {search && <span className="search-white-2">{search}</span>}
-        </h1>
-        {podcastsData ? (
-          <>
-            {podcastsData.length > 0 ? (
-              <div className="podcast_grid">
-                {podcastsData?.map(
-                  ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
-                    <PodcastCard
-                      key={_id}
-                      imgUrl={imageUrl!}
-                      title={podcastTitle}
-                      description={podcastDescription}
-                      podcastId={_id}
-                    />
-                  )
-                )}
-              </div>
-            ) : (
-              <EmptyState title="No results found" />
-            )}
-          </>
-        ) : (
+    <Suspense
+      fallback={
+        <div>
           <LoaderSpinner />
-        )}
-      </div>
-    </div>
+        </div>
+      }
+    >
+      <DiscoverClient />
+    </Suspense>
   )
 }
-
-export default Discover
